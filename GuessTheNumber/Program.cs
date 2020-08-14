2be72adeda4aebea3341f2dc56d0c043c8da4340
Jon <jon.gh@ki-games.com>
2020-08-13 19:11:03 -0700

    more

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -2,6 +2,7 @@
 /// <summary>
 /// An Unimaginative Guess the number engine
 /// Deliberatly dumb.
+/// With some deliberate logic
 /// </summary>
 namespace GuessTheNumber
 {
@@ -10,14 +11,25 @@ namespace GuessTheNumber
         static void Main(string[] args)
         {
             Random nGenerator = new Random(123);
-            int myNumber = nGenerator.Next(20);
+            //Some error trapping
+            if (args.Length == 0)
+            {
+                Console.WriteLine("GuessTheNumber requires 2 parameters the first is the number of guesses, the second is the max number I pick from.");
+                return;
+            }
+
+            int numberOfTries = int.Parse(args[0]);
+            int ceiling = int.Parse(args[1]);
+
+
+            int myNumber = nGenerator.Next(ceiling);
 
             string playerGuess="";
             int playerGuessNum = 0;
             Console.WriteLine("I am thinking of a whole number between 0 and 20");
             Console.WriteLine("Can you try and guess it in less than 5 tries ?");
 
-            for(int i = 5; i > 0 && playerGuessNum!=myNumber; i--)
+            for(int i = numberOfTries; i > 0 && playerGuessNum!=myNumber; i--)
             {
                 Console.WriteLine("You have " + i.ToString() + " tries left.");
                 Console.WriteLine("Take a guess ?");
@@ -33,9 +45,10 @@ namespace GuessTheNumber
                 }
                 else
                 {
-                    Console.WriteLine("Well Done.");
+                    Console.WriteLine($"Well Done. You took {i} attempts.");
                 }             
             }
+            Console.WriteLine("Too bad you didn't guess it, I'm just too smart.");
         }
     }
 }
