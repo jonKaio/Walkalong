24a4145f1b8e621f95fbda0d0ac21195aab4dab0
Jon <jon.gh@ki-games.com>
2020-08-14 12:59:55 -0700

    A little more

--- Walkalong/Program.cs
+++ Walkalong/Program.cs
@@ -6,7 +6,34 @@ namespace Walkalong
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MyEg myEg = new MyEg();
+            myEg.Name = "Freddie";
+
+            Console.WriteLine("Hello World!"+myEg.Name);
         }
     }
+
+    class MyEg
+    {
+        private float gravity = -9.81f;
+        public float Gravity { get => gravity; set => gravity = value; }
+
+        public int myInt = 25;
+        private string name;
+        public string Name
+        {
+            //get
+            //{
+            //    return name;
+            //}
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    name = value;
+                }
+            }
+        }
+    }
+
 }
